6921161d3127ec25ac5886ab5697ed26056a845d
lcs <lcs_27@163.com>
2021-03-11 07:15:42 +0100

    test

--- project/test.py
+++ project/test.py
@@ -1,2 +1,9 @@
 import numpy as np
-np.savetxt("./project/results/test.txt",[200,100,0])
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--task', required=True, help='The number of tasks')
+args = parser.parse_args()
+task_number: int = args.task
+
+np.savetxt("./project/results/test"+str(task_number)+".txt",np.random.normal(5, 1, 5))
